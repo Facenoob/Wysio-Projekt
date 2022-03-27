54387619f2f833b2c43636616350968a47639b43
Adam Polnik <noma0022@gmail.com>
2022-03-27 16:52:14 +0200

    A projetmunka alapja.

---
+++ src/main/java/hu/polnikadam/wysio_kocsma/WysioKocsmaApplication.java
@@ -0,0 +1,13 @@
+package hu.polnikadam.wysio_kocsma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WysioKocsmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WysioKocsmaApplication.class, args);
+    }
+
+}
