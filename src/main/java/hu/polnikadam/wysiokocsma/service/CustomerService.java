84f595474b858fce2231b1bd141291d64e83d4e6
Adam Polnik <noma0022@gmail.com>
2022-05-25 20:05:54 +0200

    WK-21 DTO.
    CustomerDTO implementálás a getAllCustomer függvényhez.

--- src/main/java/hu/polnikadam/wysiokocsma/service/CustomerService.java
+++ src/main/java/hu/polnikadam/wysiokocsma/service/CustomerService.java
@@ -1,21 +1,43 @@
 package hu.polnikadam.wysiokocsma.service;
 
+import hu.polnikadam.wysiokocsma.DTO.CustomerDTO;
 import hu.polnikadam.wysiokocsma.model.Customer;
 import hu.polnikadam.wysiokocsma.repository.CustomerRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public List<Customer> getAllCustomer() {
-        return customerRepository.findAll();
+    public List<CustomerDTO> getAllCustomer(){
+        return customerRepository.findAll()
+                .stream()
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
+
     }
+    private CustomerDTO convertEntityToDto(Customer customer){
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO = modelMapper.map(customer, CustomerDTO.class);
+        return customerDTO;
+    }
+
+
+//    public List<Customer> getAllCustomer() {
+//        return customerRepository.findAll();
+//    }
 
     public Customer getCustomerById(Long id) {
         return customerRepository.findById(id).get();
