da2b9b946f177d5f6082ea8d6b52e203418080d3
Adam Polnik <noma0022@gmail.com>
2022-05-30 16:35:40 +0200

    WK-21 DTO pr.
    Bug fix valós értékeket adjon vissza ne null-t

--- src/main/java/hu/polnikadam/wysiokocsma/converter/CustomerConverter.java
+++ src/main/java/hu/polnikadam/wysiokocsma/converter/CustomerConverter.java
@@ -4,16 +4,19 @@ import hu.polnikadam.wysiokocsma.DTO.CustomerDTO;
 import hu.polnikadam.wysiokocsma.model.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Function;
+
 
 @Component
 public class CustomerConverter {
+
     public CustomerDTO toDto(Customer customer) {
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setId(customer.getId());
         customerDTO.setName(customer.getName());
         customerDTO.setLiverStrength(customer.getLiverStrength());
         customerDTO.setBicepsSize(customer.getBicepsSize());
-        return new CustomerDTO();
+        return customerDTO;
     }
 
     public Customer toEntity(CustomerDTO customerDTO) {
